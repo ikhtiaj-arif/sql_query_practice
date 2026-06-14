550c50dde5914acfe7f905753434cb4cbeb01859
Md Ikhtiaj Arif <md.ikhtiajarif@gmail.com>
2026-06-14 18:23:47 +0600

    Fix SQL syntax for match status and add user query

--- QUERY.sql
+++ QUERY.sql
@@ -100,4 +100,14 @@ FROM
     matches
 WHERE
     tournament_category = 'Champions League'
-    AND match_status = 'Available'
+    AND match_status = 'Available';
+
+
+-- Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
+SELECT * from users 
+WHERE full_name ILIKE 'Tanvir%'
+  OR full_name ILIKE '%Haque';
+
+
+
+
